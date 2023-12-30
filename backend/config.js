0de4936d8f935f90b22949154ef2684f6c752e54
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-30 14:01:27 +0530

    setup mongoose and database connection

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5000;
+
+export const mongoDBURL = 'mongodb+srv://yatendrayadav2832:yatendra@cluster0.ldyncxi.mongodb.net/book_store?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
-import { PORT } from './config.js'
-
+import { PORT, mongoDBURL } from './config.js'
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+app.get('/', (req, res) => {
+    return res.status(200).send('<h1>Hello World!</h1>');
 })
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('connected to database successfully');
+        app.listen(PORT, () => {
+            console.log(`server running on port ${PORT}`);
+        })
+    })
+    .catch((err) => console.log(err))
+
