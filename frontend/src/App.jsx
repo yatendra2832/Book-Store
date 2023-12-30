c2b5b38132e6b28d33ab6fc2bee4491b2b2d533c
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-30 22:18:53 +0530

    installed react router dom and creating SPA and routes pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import DeleteBook from "./pages/DeleteBook";
+import CreateBook from "./pages/CreateBook";
+import EditBook from "./pages/EditBook";
+import ShowBook from "./pages/ShowBook";
 const App = () => {
-  return <h1 className="bg-red-400 text-center">Hello With Frontend</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+    </Routes>
+  );
 };
 
 export default App;
