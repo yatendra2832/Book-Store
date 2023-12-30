676ea3404ededd797027749c1b15815d2aead939
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-30 20:35:14 +0530

    setup the cors policy

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const { PORT, mongoDBURL } =require('./config.js');
+const { PORT, mongoDBURL } = require('./config.js');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 
 const app = express();
+
+// app.use(cors());//it will allow all origins with default of cors
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type"]
+    })
+);
 
 // routes 
 const bookRoute = require('./Router/book-router');
@@ -11,6 +21,7 @@ const bookRoute = require('./Router/book-router');
 // Middlewares
 app.use(express.json()); // Middleware for parsing request body
 app.use('/api', bookRoute); // Middleware for routes
+
 
 // Database connection
 mongoose
