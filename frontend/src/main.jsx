ace2abca6f66023f578552e4bfe1436499089b35
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-31 15:00:58 +0530

    React toastofy for beutiful Alert in the UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+   <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        limit={10}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     <App />
   </BrowserRouter>
 );
